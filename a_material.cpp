1c9a5e43b745abb6d00126483759430a060c361e
John Calsbeek <john.calsbeek@gmail.com>
2015-06-18 23:21:00 -0500

    Initial GGX-Smith BRDF. WIP.

--- a_material.cpp
+++ a_material.cpp
@@ -133,3 +133,81 @@ BsdfSample lambert_brdf_sample(Material const& material, Vec3 const normal, Vec3
 	bsdf_sample.probability_density = lambert_brdf_probability_density(normal, world_direction);
 	return bsdf_sample;
 }
+
+float fresnel_exact(float const ior_incoming, float const ior_outgoing, float const i_dot_h)
+{
+	float const n = ior_outgoing / ior_incoming;
+	float const c = i_dot_h;
+	float const g_squared = n*n + c*c - 1.f;
+
+	if (g_squared < 0.f) return 1.f; // total internal reflection
+
+	float const g = sqrtf(g_squared);
+	float const gpc = g + c;
+	float const gmc = g - c;
+
+	float const a = gmc / gpc;
+	float const b = (c*gpc - 1.f) / (c*gmc + 1.f);
+	return 0.5f * (a*a) * (1.f + b*b);
+}
+
+float ggx_smith_geometry_term(float const cosine, float const alpha)
+{
+	float const alpha_squared = alpha * alpha;
+	float const lerped = alpha_squared + (1.f - alpha_squared) * cosine * cosine;
+	return (2.f * cosine) / (cosine + sqrtf(lerped));
+}
+
+float ggx_smith_geometry_uncorrelated(float const n_dot_i, float const n_dot_o, float const alpha)
+{
+	return ggx_smith_geometry_term(n_dot_i, alpha) * ggx_smith_geometry_term(n_dot_o, alpha);
+}
+
+float ggx_smith_normal_density(float const n_dot_h, float const alpha)
+{
+	float const inv_pi = 0.318309886183790671538f;
+
+	float const alpha_squared = alpha * alpha;
+	float const denom = n_dot_h * n_dot_h * (alpha_squared - 1.f) + 1.f;
+	return inv_pi * alpha_squared / (denom * denom);
+}
+
+RGB ggx_smith_brdf_reflectance(Material const& material, Vec3 const normal, Vec3 const incoming, Vec3 const outgoing)
+{
+	Vec3 const h = normalize(incoming + outgoing);
+
+	float const ior_incoming = 1.0002926f; // air
+	float const ior_outgoing = 1.44f; // aluminum (roughly)
+	float const alpha = 0.5f; // completely arbitrary
+	(void)material;
+
+	float const n_dot_i = dot(normal, incoming);
+	float const n_dot_o = dot(normal, outgoing);
+	float const n_dot_h = dot(normal, h);
+	float const i_dot_h = dot(incoming, h);
+
+	float const fresnel = fresnel_exact(ior_incoming, ior_outgoing, i_dot_h);
+	float const geometry = ggx_smith_geometry_uncorrelated(n_dot_i, n_dot_o, alpha);
+	float const density = ggx_smith_normal_density(n_dot_h, alpha);
+
+	float const reflectance = (fresnel * geometry * density) / (4.f * n_dot_i * n_dot_o);
+	return RGB(reflectance, reflectance, reflectance); // TODO: non-metals
+}
+
+float ggx_smith_brdf_probability_density(Vec3 const normal, Vec3 const direction)
+{
+	return cosine_hemisphere_probability_density(normal, direction);
+}
+
+BsdfSample ggx_smith_brdf_sample(Vec3 const outgoing, Material const& material, Vec3 const normal, Vec3 const tangent, float const u1, float const u2)
+{
+	Mat33 const world_from_local(tangent, cross(normal, tangent), normal);
+	Vec3 const local_incoming = cosine_hemisphere_sample(u1, u2);
+	Vec3 const world_incoming = inv_ortho_transform_vector(world_from_local, local_incoming);
+	
+	BsdfSample bsdf_sample;
+	bsdf_sample.direction = world_incoming;
+	bsdf_sample.reflectance = ggx_smith_brdf_reflectance(material, normal, world_incoming, outgoing);
+	bsdf_sample.probability_density = ggx_smith_brdf_probability_density(normal, world_incoming);
+	return bsdf_sample;
+}
